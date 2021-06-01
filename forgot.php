f7dc3b4deed6ae15839bb659c87841b30c27c0a0
Tran Huy Khanh.github.io <57167892+HuyKhanhTran@users.noreply.github.com>
2021-06-01 22:39:56 +0700

    update code

---
+++ forgot.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../style/Style-account.css">
+    <link rel="stylesheet" href="../style/style-header-footer.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="grid">
+    <div class="header">
+        <div class="logo">
+            <a href="../index.html"><img src="../img/logo.png" alt="logo"></a>
+        </div>
+        <div class="slogan">
+            <p>Horange Mall</p>
+        </div>
+        <div class="nav-bar">
+           <div class="item">
+               <a href="../index.html">Home</a>
+           </div>
+           <div class="item">
+               <a href="../sub/aboutus.html">About Us</a>
+           </div>
+           <div class="item">
+               <a href="../sub/fees.html">Fees</a>
+           </div>
+           <div class="item">
+               <a href="../sub/contact.html">Contact</a>
+           </div>
+           <div class="item">
+               <a href="../sub/faqs.html">FAQs</a>
+           </div>
+           <div class="item">
+               <a href="../sub/login.html">My Account</a>
+           </div>
+           <div class="item">
+                       <a href="sub/admin.php">Admin Login</a>
+                   </div>
+           <div class="dropdown">
+               <a href="" class="dropbtn">Browse</a>
+               <div class="dropdown-content">
+                    <a href="../sub/browsebyname.html">Browse By Name</a>
+                    <a href="../sub/browsebycategory.html">Browse By Category</a>
+               </div>
+           </div>
+           <div class="item">
+            <a href="shopping-cart.html">Shopping cart</a> 
+        </div>
+    </div>
+  </div>
+    <div class="sign-form">
+      <img src="../img/logo.png" alt="logo">
+      <form class="form-wrap" action="" method="post">
+        <h3>Forgot password</h3>
+        <h6>Enter your email or phone number to search for your account</h6>
+          <input type="text" class = "fg-box" placeholder="Enter your email or phone number">
+          <br>
+          <button type="button" class="sign-button">Send</button>
+          </form>
+          <p><span >Got your account? Go back to</span><a href = "login.php" class = "und" > the login page</a></p>
+          <br>
+
+    </div>
+  </div>
+
+  <div class="footer">
+      <div class="copyrights">
+          <a href="../sub/copyrights.html"><span>&copy;</span>Horange. All Rights Reserved</a>
+      </div>
+      <div class="tos">
+          <a href="../sub/tos.html">Terms of Service</a>
+      </div>
+      <div class="privacy">
+          <a href="../sub/privacy.html">Privacy Policies</a>
+      </div>
+  </div>
+</div>
+</body>
+<div class="cookie-container">
+    <p>We use cookies in this website to give you the best experience. Please accept our <a href="https://gdpr-info.eu/"> Terms and conddition</a> before using our site .</p>
+    <button class="cookie-btn">
+        Agree
+    </button>
+</div>
+<script src="../jvs/cookies.js"></script>
+</html>
---
+++ templates/include/footer.php
@@ -0,0 +1,19 @@
+<div class="grid">
+        <div class="footer">
+            <div class="copyrights">
+                <a href="../sub/copyrights.html"><span>&copy;</span>Horange. All Rights Reserved</a>
+            </div>
+            <div class="tos">
+                <a href="../sub/tos.html">Terms of Service</a>
+            </div>
+            <div class="privacy">
+                <a href="../sub/privacy.html">Privacy Policies</a>
+            </div>
+        </div>
+    </div>
+    <script src='https://cdn.jsdelivr.net/g/lodash@4(lodash.min.js+lodash.fp.min.js)'></script>
+    <script src="../jvs/login.js"></script>
+    <script src="../jvs/logged.js"></script>
+  </body>
+</html>
+
